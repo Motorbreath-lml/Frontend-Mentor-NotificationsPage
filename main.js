dcec20ab3fd6ae33078069180565bcc79dc21310
Carlos Hernandez <al2133034127@azc.uam.mx>
2024-03-29 11:56:06 -0600

    Proyecto terminado en freeCodeCamp Español

---
+++ main.js
@@ -0,0 +1,30 @@
+//console.log('Estajalando el JS');
+
+const markAll = document.querySelector('#mark-all');
+const numberElement = document.querySelector('#number');
+const posts = document.querySelectorAll('.post'); //traer todos los posts
+
+// Acada post, se escucha el evento de click, cuando pase se quita la clase 'not-read' de ese post en particular
+posts.forEach(post => {
+    post.addEventListener('click',()=>{
+        post.querySelector('.not-read').classList.remove('not-read');
+        updateNotifications();
+    });
+});
+
+markAll.addEventListener('click',()=>{
+    const notReadElements = document.querySelectorAll('.not-read');
+    //console.log(notReadElements);
+    notReadElements.forEach(element => {
+        element.classList.remove('not-read');
+    });
+
+    updateNotifications();
+});
+
+//Actualizar el conteno de notificaciones
+const updateNotifications = ()=>{
+    const notReadElementsActual = document.querySelectorAll('.not-read');
+    // console.log(notReadElementsActual.length);
+    numberElement.innerText=notReadElementsActual.length;
+};
