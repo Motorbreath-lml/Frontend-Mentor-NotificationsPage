5dcd0fd5afa67bf5281a5b929b7a168ef534a2a8
Carlos Hernandez <al2133034127@azc.uam.mx>
2024-04-01 22:37:39 -0600

    Agregue un README propio, modificacion al bg de una notificacion al ser leida

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const posts = document.querySelectorAll('.post'); //traer todos los posts
 // Acada post, se escucha el evento de click, cuando pase se quita la clase 'not-read' de ese post en particular
 posts.forEach(post => {
     post.addEventListener('click',()=>{
+        post.classList.add('read');
         post.querySelector('.not-read').classList.remove('not-read');
         updateNotifications();
     });
@@ -15,8 +16,14 @@ posts.forEach(post => {
 markAll.addEventListener('click',()=>{
     const notReadElements = document.querySelectorAll('.not-read');
     //console.log(notReadElements);
-    notReadElements.forEach(element => {
-        element.classList.remove('not-read');
+    notReadElements.forEach(element => {        
+        element.classList.remove('not-read');        
+    });
+
+    posts.forEach(post => {
+        if(!post.classList.contains('read')){
+            post.classList.add('read');
+        }
     });
 
     updateNotifications();
